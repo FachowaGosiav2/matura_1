65cec25b9a3c0c38ce0f1ecf4340940709807d43
FachowaGosiav2 <gracjan.broniecki@gmail.com>
2023-03-09 10:07:19 +0100

    zadanie_2

--- zd_1_3.py
+++ zd_1_3.py
@@ -1,15 +1,22 @@
-with open('mecz_przyklad.txt', 'r') as f:
+with open('mecz.txt', 'r') as f:
     contents = f.read()
-    passa, i, ilosc, passa_max = 0, 0, 0, 0
+    passa, i, ilosc, passa_max, a, b, win = 1, 0, 0, 0, 0, 0, ''
     
     while i < len(contents) - 1:
 
         if contents[i] == contents[i + 1]:
             passa += 1
 
-        if contents[i] != contents[i + 1] and passa:
+        if passa >= 10 and passa_max < passa:
+            win = contents[i]
             passa_max = passa
-            passa = 0
+        
+        if contents[i] != contents[i + 1] and passa >= 10:
+            ilosc += 1
+
+        if contents[i] != contents[i + 1]:
+            passa = 1
+
         i += 1
     
-print(passa_max)
+print(ilosc, win, passa_max)
